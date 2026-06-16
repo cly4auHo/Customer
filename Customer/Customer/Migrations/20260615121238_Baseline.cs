fb0a8233695c2f83d620998a28ea46d0b75db407
Arsen <arsen.dmitriiev@gmail.com>
2026-06-16 08:44:08 +0300

    add migration

---
+++ Customer/Customer/Migrations/20260615121238_Baseline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Customer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Baseline : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Currency");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
